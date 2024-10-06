5ff218d2bdcd6a62a66d1ae7261926c90be146df
Fay Alaamri <fay.alamri@gmail.com>
2024-10-06 19:51:59 +0300

    changes on branch

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, CI/CD with GitLab!"
+    return "Hello, CI/CD !"
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
 
-    
+
